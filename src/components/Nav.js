e24e2d2cf784b5ba8032da2df4267127913e1211
Anitha <anitha2002priya2003@gmail.com>
2023-10-14 19:56:52 +0530

    hello

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+    return (
+        <nav className = "main-nav">
+            <ul>
+                <li>Home</li>
+                <li>Articles</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+            </ul>
+        </nav>
+    );
+}
+export default Nav;
